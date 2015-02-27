9557119c0971f2e688dd6449a044864fac6094f7
Etienne Laurin <atnnn@rethinkdb.com>
2015-02-26 18:53:17 -0800

    get icu to work maybe

--- mk/support/pkg/icu.sh
+++ mk/support/pkg/icu.sh
@@ -2,3 +2,19 @@
 version=54.1
 
 src_url=http://download.icu-project.org/files/icu4c/$version/icu4c-${version/\./_}-src.tgz
+
+pkg_configure () {
+    in_dir "$build_dir/source" ./configure --prefix="$(niceabspath "$install_dir")" --enable-static "$@"
+}
+
+pkg_make () {
+    in_dir "$build_dir/source" make "$@"
+}
+
+pkg_install-include () {
+    # pkg_configure
+    # The install-headers-recursive target is missing. Let's patch it.
+    sed 's/distclean-recursive/install-headers-recursive/g;$a\install-headers-local:' -i "$build_dir/source/Makefile"
+    sed '$a\install-headers:' -i "$build_dir"/source/*/Makefile
+    pkg_make install-headers-recursive
+}
